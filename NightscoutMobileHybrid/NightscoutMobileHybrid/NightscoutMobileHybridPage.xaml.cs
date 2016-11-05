a0b07028f62f80f7227d3bb5c87443dd43e78fd4
Andrew Ditmer <andrew@ditmer.us>
2016-11-04 22:11:13 -0400

    Initial commit and update to Xamarin.Forms packages

---
+++ NightscoutMobileHybrid/NightscoutMobileHybrid/NightscoutMobileHybridPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace NightscoutMobileHybrid
+{
+	public partial class NightscoutMobileHybridPage : ContentPage
+	{
+		public NightscoutMobileHybridPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
