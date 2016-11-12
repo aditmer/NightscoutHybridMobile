3c1f75223b5a388278dc3fa2bacbea67d4f0f9be
Andrew Ditmer <andrew@ditmer.us>
2016-11-12 00:30:02 -0500

    adds empty PushNotificatioinImplementation on Android

---
+++ NightscoutMobileHybrid/Droid/PushNotificationImplementation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace NightscoutMobileHybrid.Droid
+{
+	public class PushNotificationImplementation : Java.Lang.Object, IPushNotifications
+	{
+		public PushNotificationImplementation()
+		{
+		}
+
+		public void Register()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Unregister()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
