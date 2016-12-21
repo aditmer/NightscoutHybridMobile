aa89c6e68caf4808c00ed3be9141f0372e40efbf
Andrew Ditmer <andrew@ditmer.us>
2016-12-21 15:37:06 -0500

    Increases font size on buttons on iOS and matches button styles on both pages.  Adds a reachability call to the URL and shows a nicer error message if it can’t be reached.

--- NightscoutMobileHybrid/NightscoutMobileHybrid/NightscoutMobileHybridPage.xaml.cs
+++ NightscoutMobileHybrid/NightscoutMobileHybrid/NightscoutMobileHybridPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Lamp.Plugin;
+using Plugin.Connectivity;
 using Xamarin.Forms;
 
 namespace NightscoutMobileHybrid
@@ -17,7 +18,7 @@ namespace NightscoutMobileHybrid
 
 			if (ApplicationSettings.URL != string.Empty)
 			{
-				wvNightscout.Source = ApplicationSettings.URL;
+				TryURL(7000);
 			}
 			else
 			{
@@ -27,7 +28,7 @@ namespace NightscoutMobileHybrid
 
 			MessagingCenter.Subscribe<SettingsPage>(this, "URLChanged", (SettingsPage obj) =>
 			{
-				wvNightscout.Source = ApplicationSettings.URL;
+				TryURL(7000);
 			});
 
 			MessagingCenter.Subscribe<Exception,string>(this, "Register Error", (page,errorMessage) =>
@@ -43,6 +44,22 @@ namespace NightscoutMobileHybrid
 			//swScreenLockOverride.Toggled += SwScreenLockOverride_Toggled;
 		}
 
+		private async void TryURL(int timeoutInMilliseconds = 5000)
+		{
+			if (await CrossConnectivity.Current.IsRemoteReachable(ApplicationSettings.URL, 80, timeoutInMilliseconds))
+			{
+				wvNightscout.Source = ApplicationSettings.URL;
+			}
+			else
+			{
+				var htmlSource = new HtmlWebViewSource();
+				htmlSource.Html = $"<html><body style=\"background-color:#000; color:fff\"><div ><p>We could not reach the URL {ApplicationSettings.URL}. &nbsp;Please check your URL for typos and make sure you are online.</p>\n<p>&nbsp;</p>\n<p>This is not the Nightscout you are looking for.</p></div></body></html>";
+				wvNightscout.Source = htmlSource;
+			}
+
+		}
+		//Autohide native controls when in landscape orientation
+		//Also force a refresh to try to eleminate some rendering issues - added on 12/21/16
 		protected override void OnSizeAllocated(double width, double height)
 		{
 			base.OnSizeAllocated(width, height); // Important!
@@ -54,6 +71,7 @@ namespace NightscoutMobileHybrid
 
 				grdNativeControls.IsVisible = (width < height);
 
+				TryURL(7000);
 			}
 		}
 
